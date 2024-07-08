79157c887889da9ba93292ab0958b79ba9427ef5
aaapwn <armmykung2547@gmail.com>
2024-07-08 15:18:30 +0700

    add week2

---
+++ Week-2/mysite/polls/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
